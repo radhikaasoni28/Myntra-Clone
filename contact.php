cdf0bc66ffa33d62ade9955868399c58be118ebc
radhikaasoni28 <108170717+radhikaasoni28@users.noreply.github.com>
2022-10-19 23:35:51 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link rel="stylesheet" href="contact.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <title>Contact</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        </head>
+
+    <body>
+        
+        <section class="section1 contact">
+                <div class="content">
+                        <h2> Contact Us  </h2>
+                </div>
+                <div class="contain">
+                        <div class="contactinfo">
+                                <div class="box">
+                                        <div class="icon"> <i class="fa fa-map-marker" aria-hidden="true"></i></div>
+                                        <div class="txt">
+                                                <h3>Address</h3>
+                                                <p>XYZ<br>1250838
+                                                </p>
+                                        </div>
+                                </div>
+
+                                <div class="box">
+                                    <div class="icon"><i class="fa fa-phone" aria-hidden="true"></i></div>
+                                    <div class="txt">
+                                            <h3>Phone </h3>
+                                            <p>+91 8168945940</p>
+                                    </div>
+                                </div>
+                                <div class="box">
+                                    <div class="icon"><i class="fa fa-envelope" aria-hidden="true"></i></div>
+                                    <div class="txt">
+                                            <h3>Email </h3>
+                                            <p>radhika@gmail.com</p>
+                                    </div>
+                                </div>
+                        </div>
+                        <div class="contactForm">
+                            <form action="">
+                                <h2>Send Message</h2>
+                                <div class="inputBox">
+                                    <input type="text" required="required">
+                                    <span>Full Name </span>
+                                </div>
+                                <div class="inputBox">
+                                    <input type="text"  required="required">
+                                    <span>Mobile Number </span>
+                                </div>
+                                <div class="inputBox">
+                                    <input type="text"  required="required">
+                                    <span>Email </span>
+                                </div>
+                                <div class="inputBox">
+                                    <label for="cars">Country : </label>
+                                    <select name="cars" id="cars">
+                                                <option value="">---- Select ----</option>
+                                                <option value="India">India</option>
+                                                <option value="">Australia</option>
+                                                <option value="Dubai">Dubai</option>
+                                                <option value="">Japan</option>
+                                                <option value="">Kuwait</option>
+                                                <option value="">Singapore</option>
+                                                <option value="">South Korea</option>  
+                                                <option value="">UK</option>                                             
+                                                <option value="US">US</option>                                                                                                
+                                    </select>
+                                            
+                                </div>
+
+                                <div class="inputBox">
+                                  <input type="text"  required="required">
+                                    <span>Type your message... </span>
+                                </div>
+
+                                
+                                <div class="inputBox" >
+                                    <input type="submit" value="Send" required="required">
+                                </div>
+                            </form>
+                        </div>
+                </div>
+        </section>
+
+        <section class="section2">
+
+        </section>
+    </body>
+</html>
